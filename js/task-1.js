8028df503b1e92a11f4a5ec0a014da57d9818220
vito <vito333@ukr.net>
2025-05-18 11:12:44 +0300

    hw-07

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,12 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const categories = document.querySelectorAll(".item");
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(function(category) {
+    const title = category.querySelector("h2").textContent;
+    const itemContent = category.querySelectorAll("li").length;
+
+console.log(`Category: ${title}`);
+console.log(`Elements: ${itemContent}`);
+});
+
--- js/task-3.js
+++ js/task-3.js
@@ -1,31 +1,7 @@
-class StringBuilder {
-  #value;
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+input.addEventListener("input", () => {
+  const inputValue = input.value.trim();
+  output.textContent = inputValue === "" ? "Anonymous" : inputValue;
+});
